4c5bc0f1945428730478d410dd52facf8fea4618
Ivan Todorov <iwan.todorow@gmail.com>
2021-08-31 19:19:50 +0300

    Exercises-From-ConditionalStatements-Lab

---
+++ C#ProgrammingBasics/2.ConditionalStatements/ConditionalStatements/AreasOfFigures/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AreasOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure =Console.ReadLine();
+
+            if (figure == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine($"{a * a:f2}");
+            }
+            else if (figure == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"{a * b:f3}");
+            }
+            else if (figure == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"{Math.PI * r *r:f3}");
+            }
+            else if (figure == "triangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double h = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"{a * h / 2:f3}");
+            }
+        }
+    }
+}
---
+++ C#ProgrammingBasics/2.ConditionalStatements/ConditionalStatements/ToyStore/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ToyStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pTrip = double.Parse(Console.ReadLine());
+            int cPuzzle = int.Parse(Console.ReadLine());
+            int cDolls = int.Parse(Console.ReadLine());
+            int cBears = int.Parse(Console.ReadLine());
+            int cMin = int.Parse(Console.ReadLine());
+            int cTrucks = int.Parse(Console.ReadLine());
+
+            double pPuzzle = cPuzzle * 2.60;
+            double pDolls = cDolls * 3;
+            double pBear = cBears * 4.10;
+            double pMin = cMin * 8.20;
+            double pTrucks = cTrucks * 2;
+
+            double pALL = pPuzzle + pDolls + pBear + pMin + pTrucks;
+
+            if (cPuzzle + cDolls + cBears + cMin + cTrucks >= 50)
+            {
+                double pDisc = pALL * 0.75 * 0.90;
+
+                if (pDisc >= pTrip)
+                {
+                    double left = pDisc - pTrip;
+                    Console.WriteLine($"Yes! {left:f2} lv left.");
+                }
+                else if (pDisc <= pTrip)
+                {
+                    double missing = pTrip - pDisc;
+                    Console.WriteLine($"Not enough money! {missing:f2} lv needed.");
+                }
+            }
+             else if (cPuzzle + cDolls + cBears + cMin + cTrucks < 50)
+            {
+                double pDisc = pALL * 0.90;
+
+                if (pDisc >= pTrip)
+                {
+                    double left = pDisc - pTrip;
+                    Console.WriteLine($"Yes! {left:f2} lv left.");
+                }
+                else if (pDisc <= pTrip)
+                {
+                    double missing = pTrip - pDisc;
+                    Console.WriteLine($"Not enough money! {missing:F2} lv needed.");
+                }
+            }
+        }
+    }
+}
