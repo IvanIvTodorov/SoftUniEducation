001328b2541b2b525e094cebd039cc429ceef4b5
Ivan Todorov <iwan.todorow@gmail.com>
2021-09-17 18:46:15 +0200

    Exercises-From-Arrays-MoreExercise

--- C#FundamentalsModule/3.Arrays/ArraysMoreExercises/LongestIncreasingSubsequence/Program.cs
+++ C#FundamentalsModule/3.Arrays/ArraysMoreExercises/LongestIncreasingSubsequence/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace LongestIncreasingSubsequence
@@ -7,11 +8,46 @@ namespace LongestIncreasingSubsequence
     {
         static void Main(string[] args)
         {
-            int[] nums = Console.ReadLine()
-                .Split()
+            List<int> listOfDigit = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
-                .ToArray();
+                .ToList();
 
+            int[] bestList;
+
+            List<int> len = new List<int>();
+            List<int> prev = new List<int>();
+
+            int maximum = 0;
+            int lastIndex = -1;
+
+            for (int i = 0; i < listOfDigit.Count; i++)
+            {
+                len.Add(1);
+                prev.Add(-1);
+                for (int j = 0; j < i; j++)
+                {
+                    if (listOfDigit[j] < listOfDigit[i] && len[j] >= len[i])
+                    {
+                        len[i] = 1 + len[j];
+                        prev[i] = j;
+                    }
+                }
+                if (len[i] > maximum)
+                {
+                    maximum = len[i];
+                    lastIndex = i;
+                }
+            }
+            bestList = new int[maximum];
+            for (int i = 0; i < bestList.Length; i++)
+            {
+                bestList[i] = listOfDigit[lastIndex];
+                lastIndex = prev[lastIndex];
+            }
+            Array.Reverse(bestList);
+
+            Console.WriteLine(string.Join(" ", bestList));
         }
     }
 }
