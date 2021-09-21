b1f684327c4377208df48b7172febc6c158c5694
Ivan Todorov <iwan.todorow@gmail.com>
2021-09-21 20:27:27 +0200

    Exercises-From-Generics-Lab

---
+++ C#Advanced/6.Generics/Generics-Lab/GenericScale/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericScale
+{
+    class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var result = new EqualityScale<int>(5 , 6);
+
+            Console.WriteLine(result.AreEqual());
+        }
+    }
+}
