18b7527e83d912bc6d70110d06ddf2713498fd78
Ivan Todorov <iwan.todorow@gmail.com>
2021-09-21 19:41:45 +0200

    Exercises-From-Generics-Lab

---
+++ C#Advanced/6.Generics/Generics-Lab/BoxOfT/Box.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private  readonly List<T> elements;
+
+        public Box()
+        {
+            elements = new List<T>();
+        }
+        public int Count 
+        {
+           get
+            {
+                return elements.Count;
+            }
+                
+        }
+        public void Add(T element)
+        {
+
+            elements.Add(element);
+        }
+
+        public T Remove()
+        {
+            T remove = elements.LastOrDefault();
+            elements.Remove(remove);
+
+            return remove;
+        }
+    }
+}
