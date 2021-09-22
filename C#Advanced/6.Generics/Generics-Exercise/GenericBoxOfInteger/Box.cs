12fe2220090819c3d77aad5eacb99c2a107c1e44
Ivan Todorov <iwan.todorow@gmail.com>
2021-09-22 19:37:13 +0200

    Exercises-From-Generic-Exercise

---
+++ C#Advanced/6.Generics/Generics-Exercise/GenericBoxOfInteger/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        public readonly T value;
+
+        public Box(T value)
+        {
+            this.value = value;
+        }
+        public override string ToString()
+        {
+            return $"{this.value.GetType()}: {this.value}";
+        }
+    }
+}
---
+++ C#Advanced/6.Generics/Generics-Exercise/GenericBoxOfInteger/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericBoxOfInteger
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <  num; i++)
+            {
+                string input = Console.ReadLine();
+
+                var box = new Box<int>(int.Parse(input));
+
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
