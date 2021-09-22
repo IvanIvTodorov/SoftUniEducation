0cdc0bd9ad60f5131bae6fd846abbd5cec736b8b
Ivan Todorov <iwan.todorow@gmail.com>
2021-09-22 19:37:59 +0200

    Exercises-From-Generic-Exercise

---
+++ C#Advanced/6.Generics/Generics-Exercise/GenericCountMethodString/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenericCountMethodString
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        public static int Compare(T value, List<T> list)
+        {
+            int count = 0;
+
+            foreach (var item in list)
+            {
+                if (item.CompareTo(value) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ C#Advanced/6.Generics/Generics-Exercise/GenericCountMethodString/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodString
+{
+    public  class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < number; i++)
+            {
+                string input = Console.ReadLine();
+
+                list.Add(input);
+            }
+
+            string compare = Console.ReadLine();
+
+            Console.WriteLine(Box<string>.Compare(compare, list));
+        }
+    }
+}
