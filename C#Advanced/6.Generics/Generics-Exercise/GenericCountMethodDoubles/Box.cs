e7d400caf37200ecc44f8affa0ff057a3bebd1f6
Ivan Todorov <iwan.todorow@gmail.com>
2021-09-22 19:38:11 +0200

    Exercises-From-Generic-Exercise

---
+++ C#Advanced/6.Generics/Generics-Exercise/GenericCountMethodDoubles/Box.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenericCountMethodDoubles
+{
+    public class Box<T> where T : IComparable<T>
+    {
+
+        public static double  Comparer(T value, List<T> list)
+        {
+            return list.Where(n => n.CompareTo(value) > 0).Count();
+
+        }
+    }
+}
---
+++ C#Advanced/6.Generics/Generics-Exercise/GenericCountMethodDoubles/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodDoubles
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            List<double> list = new List<double>();
+
+            for (int i = 0; i < numbers; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+
+                list.Add(input);
+            }
+
+            double comparer = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Box<double>.Comparer(comparer, list));
+        }
+    }
+}
