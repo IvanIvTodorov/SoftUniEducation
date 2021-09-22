c849fe3a40b34f330a44d26927362b0c4fefe6af
Ivan Todorov <iwan.todorow@gmail.com>
2021-09-22 19:37:29 +0200

    Exercises-From-Generic-Exercise

---
+++ C#Advanced/6.Generics/Generics-Exercise/GenericSwapMethodStrings/MyList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericSwapMethodStrings
+{
+    public class MyList<T>
+    {
+
+        public MyList()
+        {
+            List = new List<T>();
+        }
+        public List<T> List { get; set; }
+        public List<T> SwapElements(int firstIndex, int secondIndex)
+        {
+
+            T swap = this.List[secondIndex];
+            this.List[secondIndex] = this.List[firstIndex];
+            this.List[firstIndex] = swap;
+
+            return this.List;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in this.List)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ C#Advanced/6.Generics/Generics-Exercise/GenericSwapMethodStrings/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericSwapMethodStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            var list = new MyList<string>();
+
+            for (int i = 0; i < number; i++)
+            {
+                string input = Console.ReadLine();
+
+                list.List.Add(input);
+
+            }
+
+            int[] indexs = Console.ReadLine()
+               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+
+            list.SwapElements(indexs[0], indexs[1]);
+
+            Console.WriteLine(list);
+
+        }
+    }
+}
