53f6ba59de00c5614e7eb7def72495632d7a7503
Ivan Todorov <iwan.todorow@gmail.com>
2021-09-22 19:38:40 +0200

    Exercises-From-Generic-Exercise

---
+++ C#Advanced/6.Generics/Generics-Exercise/Threeuple/MyThreeuple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threeuple
+{
+    public class MyThreeuple<T,U,V>
+    {
+        private readonly T first;
+        private readonly U second;
+        private readonly V third;
+
+        public MyThreeuple(T first, U second, V third)
+        {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+        }
+
+        public override string ToString()
+        {
+            return $"{first} -> {second} -> {third}";
+        }
+
+    }
+}
---
+++ C#Advanced/6.Generics/Generics-Exercise/Threeuple/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Threeuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = SplitInput();
+
+            for (int i = 4; i < input.Length; i++)
+            {
+                input[3] += $" {input[i]}";
+            }   
+
+            var threeuple = new MyThreeuple<string, string, string>(($"{input[0]} {input[1]}"), input[2], input[3]);
+
+            input = SplitInput();
+
+            var second = new MyThreeuple<string, int, bool>(input[0], int.Parse(input[1]),
+                input[2] == "drunk" ? true : false);
+
+            input = SplitInput();
+
+            var third = new MyThreeuple<string, double, string>(input[0], double.Parse(input[1]), input[2]);
+
+            Console.WriteLine(threeuple);
+            Console.WriteLine(second);
+            Console.WriteLine(third);
+
+        }
+
+        private static string[] SplitInput()
+        {
+            return Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
