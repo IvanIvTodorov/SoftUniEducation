67ef1dd689f2a3e3c77f20fb09714d3f7ae67947
Ivan Todorov <iwan.todorow@gmail.com>
2021-09-22 19:36:59 +0200

    Exercises-From-Generic-Exercise

---
+++ C#Advanced/6.Generics/Generics-Exercise/GenericBoxOfString/Box.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Value.GetType()}: {this.Value}";
+        }
+    }
+}
---
+++ C#Advanced/6.Generics/Generics-Exercise/GenericBoxOfString/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+           
+
+            for (int i = 0; i < number; i++)
+            {
+                string input = Console.ReadLine();
+
+                var box = new Box<string>();
+
+                box.Value = input;
+
+                Console.WriteLine(box);
+
+            }
+        }
+    }
+}
