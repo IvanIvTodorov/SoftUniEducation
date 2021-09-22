145d460e5b22cfd8a9bbb00aeb220e534781f613
Ivan Todorov <iwan.todorow@gmail.com>
2021-09-22 19:38:27 +0200

    Exercises-From-Generic-Exercise

---
+++ C#Advanced/6.Generics/Generics-Exercise/Tuple/MyTuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class MyTuple<T, V>
+    {
+        private readonly T firstItem;
+        private readonly V secondItem;
+
+        public MyTuple(T first, V second)
+        {
+            firstItem = first;
+            secondItem = second;
+        }
+
+        public override string ToString()
+        {
+            return $"{firstItem} -> {secondItem}";
+        }
+    }
+}
---
+++ C#Advanced/6.Generics/Generics-Exercise/Tuple/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] input = SplitInput();
+
+            MyTuple<string, string> tuple = new MyTuple<string, string>(($"{input[0]} {input[1]}"), input[2]);
+
+            input = SplitInput();
+
+            var secTuple = new MyTuple<string, int>(input[0], int.Parse(input[1]));
+
+            input = SplitInput();
+
+            var thirdTuple = new MyTuple<int, double>(int.Parse(input[0]), double.Parse(input[1]));
+
+            Console.WriteLine(tuple);
+            Console.WriteLine(secTuple);
+            Console.WriteLine(thirdTuple);
+        }
+
+        private static string[] SplitInput()
+        {
+            return Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
