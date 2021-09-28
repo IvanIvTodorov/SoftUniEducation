e2917178689a4a61a1ddac0bae3d81553e86dcf2
Ivan Todorov <iwan.todorow@gmail.com>
2021-09-28 13:00:08 +0200

    Exercises-From-IteratorsAndComparators-Exercise

---
+++ C#Advanced/7.IteratorsAndComparators/IteratorsAndComparators-Exercise/ListyIterator/MyListyIterator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class MyListyIterator<T> : IEnumerable<T>
+    {
+        private int currentIndex = 0;
+        public MyListyIterator(params T[] elements)
+        {
+            Elements = new List<T>(elements);
+        }
+
+        public int Count => this.Elements.Count;
+        public List<T> Elements { get; set; }
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+        public bool Move()
+        {
+            if (++currentIndex < this.Elements.Count)
+            {
+                return true;            
+            }
+            else
+            {
+                currentIndex--;
+                return false;
+            }          
+        }
+
+        public void Print()
+        {
+            if (this.Elements.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            else
+            {
+                Console.WriteLine(this.Elements[currentIndex]);
+            }
+        }
+
+        public bool HasNext()
+        {
+            return currentIndex < this.Elements.Count - 1 ? true : false;
+        }            
+    }
+}
---
+++ C#Advanced/7.IteratorsAndComparators/IteratorsAndComparators-Exercise/ListyIterator/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            MyListyIterator<string> myIetarator = new MyListyIterator<string>(input.Skip(1).ToArray());
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {               
+                if (command == "Move")
+                {
+                    Console.WriteLine(myIetarator.Move());
+                }
+                else if (command == "Print")
+                {
+                    try
+                    {
+                        myIetarator.Print();
+                    }
+                    catch (InvalidOperationException e)
+                    {
+
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(myIetarator.HasNext());
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
