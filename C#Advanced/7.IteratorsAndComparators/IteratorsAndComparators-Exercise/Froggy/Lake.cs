33fedebb3904d7d35157f2de6e32f4e2d53805dd
Ivan Todorov <iwan.todorow@gmail.com>
2021-09-28 14:18:50 +0200

    Exercises-From-IteratorsAndComparators-Exercise

---
+++ C#Advanced/7.IteratorsAndComparators/IteratorsAndComparators-Exercise/Froggy/Lake.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private int[] arr;
+
+        public Lake(params int[] elements)
+        {
+            this.arr = elements;
+        }
+       public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < arr.Length; i += 2)
+            {
+                yield return arr[i];
+            }
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    yield return arr[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+       
+
+
+    }
+}
