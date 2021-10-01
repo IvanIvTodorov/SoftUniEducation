a67443aec7db137c48741ae17a05d0a5e152ff02
Ivan Todorov <iwan.todorow@gmail.com>
2021-10-01 18:40:29 +0200

    Exercises-From-Inheritance-Lab

---
+++ C#OOP/Inheritance-Lab/CustomRandomList/RandomList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString(List<string> list)
+        {
+            var rnd = new Random();
+            int random = rnd.Next(0, list.Count());
+            string remove = list[random];
+            list.RemoveAt(random);
+            return remove;
+        }
+    }
+}
