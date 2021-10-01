279958fab27ca0be2012ece44dba6bb47cd64409
Ivan Todorov <iwan.todorow@gmail.com>
2021-10-01 18:27:02 +0200

    Exercises-From-Inheritance-Lab

--- C#OOP/Inheritance-Lab/Farm/StartUp.cs
+++ C#OOP/Inheritance-Lab/Farm/StartUp.cs
@@ -7,10 +7,13 @@ namespace Farm
         static void Main(string[] args)
         {
 
-            Puppy puppy = new Puppy();
-            puppy.Eat();
-            puppy.Bark();
-            puppy.Weep();
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+
+            Cat cat = new Cat();
+            cat.Eat();
+            cat.Meow();
 
         }
     }
