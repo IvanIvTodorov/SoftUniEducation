3b182b1cb5b9e6b8b0aeabb8c9b9d5dfd4c006a5
Ivan Todorov <iwan.todorow@gmail.com>
2021-10-02 11:10:16 +0200

    Exercise-From-Inheritance-Exercise

---
+++ C#OOP/Inheritance/Inheritance-Exercise/PlayersAndMonsters/Elfs/Elf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Elf : Hero
+    {
+        public Elf(string username,int level)
+            : base(username,level)
+        {
+
+        }
+
+    }
+}
