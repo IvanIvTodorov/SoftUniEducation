dbb6a3a26a8d7f5dea1c9cc17276f0e1bcf18cd3
Ivan Todorov <iwan.todorow@gmail.com>
2021-10-02 14:08:28 +0200

    Exercises-From-Inheritance-Exercise

---
+++ C#OOP/Inheritance/Inheritance-Exercise/Restaurant/Cake.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private static double grams = 250;
+        private static double calories = 1000;
+        private static decimal price = 5M;
+        public Cake(string name) 
+            : base(name, price, grams, calories)
+        {
+        }
+    }
+}
---
+++ C#OOP/Inheritance/Inheritance-Exercise/Restaurant/Coffee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const decimal price = 3.5M;
+        private const double mililiters = 50;
+        public Coffee(string name ,double caffeine) 
+            : base(name, price, mililiters)
+        {
+            Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}
