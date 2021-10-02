7db41e5f6301437d95181c0c2c4fc5bf84b9af8f
Ivan Todorov <iwan.todorow@gmail.com>
2021-10-02 11:33:42 +0200

    Exercises-From-Inheritance-Exercise

---
+++ C#OOP/Inheritance/Inheritance-Exercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+            DefaultFuelConsumption = 1.25;
+        }
+        public double DefaultFuelConsumption { get; set; }
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+
+        public virtual void Drive(double km)
+        {
+            Fuel -= km * FuelConsumption;
+        }
+    }
+}
