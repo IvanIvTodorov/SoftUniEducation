4d3ca66d074532068e0eb25022d58b23f4cf152a
Ivan Todorov <iwan.todorow@gmail.com>
2021-10-02 10:59:41 +0200

    Exercises-From-Inheritance-Exercise

---
+++ C#OOP/Inheritance/Inheritance-Exercise/Zoo/Gorilla.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Gorilla : Mammal
+    {
+        public Gorilla(string name)
+            : base(name)
+        {
+
+        }
+
+    }
+}
---
+++ C#OOP/Inheritance/Inheritance-Exercise/Zoo/Mammal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Mammal : Animal
+    {
+        public Mammal(string name)
+            : base(name)
+        {
+
+        }
+
+    }
+}
