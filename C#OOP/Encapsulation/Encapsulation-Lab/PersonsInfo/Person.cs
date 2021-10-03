7dea5fb0da66181445c35e11b124a9782a3d245e
Ivan Todorov <iwan.todorow@gmail.com>
2021-10-03 10:51:13 +0200

    Exercises-From-Encapsulation-Lab

---
+++ C#OOP/Encapsulation/Encapsulation-Lab/PersonsInfo/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age,decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; private set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age < 30)
+            {
+                Salary = Salary * percentage / 100 * 100;
+            }
+            else
+            {
+                Salary = Salary * percentage / 100 * 200;
+            }
+            
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.";
+        }
+    }
+}
