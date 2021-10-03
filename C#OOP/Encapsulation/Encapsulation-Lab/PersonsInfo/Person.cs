7c4f1d090abdaee5b4f7d81f5a4dca274274e364
Ivan Todorov <iwan.todorow@gmail.com>
2021-10-03 11:09:05 +0200

    Exercises-From-Encapsulation-Lab

--- C#OOP/Encapsulation/Encapsulation-Lab/PersonsInfo/Person.cs
+++ C#OOP/Encapsulation/Encapsulation-Lab/PersonsInfo/Person.cs
@@ -6,6 +6,10 @@ namespace PersonsInfo
 {
     public class Person
     {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
         public Person(string firstName, string lastName, int age,decimal salary)
         {
             FirstName = firstName;
@@ -13,10 +17,78 @@ namespace PersonsInfo
             Age = age;
             Salary = salary;
         }
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
-        public int Age { get; private set; }
-        public decimal Salary { get; private set; }
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set 
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                else
+                {
+                    firstName = value;
+                }
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                else
+                {
+                    lastName = value;
+                }
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (age <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                if (salary < 650)
+                {
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+                else
+                {
+                    salary = value;
+                }
+            }
+        }
 
         public void IncreaseSalary(decimal percentage)
         {
