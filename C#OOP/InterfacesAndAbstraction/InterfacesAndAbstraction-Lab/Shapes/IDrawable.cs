93072c103aa4084c5d8ef167ff08f6bf5e462a59
Ivan Todorov <iwan.todorow@gmail.com>
2021-10-05 18:27:14 +0200

    Exercises-From-InterfacesAndAbstraction-Lab

---
+++ C#OOP/InterfacesAndAbstraction/InterfacesAndAbstraction-Lab/Shapes/IDrawable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public interface IDrawable
+    {
+
+        public void Draw();
+    }
+}
---
+++ C#OOP/InterfacesAndAbstraction/InterfacesAndAbstraction-Lab/Shapes/Rectangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public void Draw()
+        {
+            DrawLine();
+            for (int i = 0; i < height - 2; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (j == 0 || j == width - 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            DrawLine();
+        }
+
+        private void DrawLine()
+        {
+            Console.WriteLine(new string('*', this.width));
+        }
+
+    }
+}
