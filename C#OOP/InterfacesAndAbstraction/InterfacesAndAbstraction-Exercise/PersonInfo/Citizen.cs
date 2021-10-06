1848071d19e102a8591a5a54d665fbfac53cf155
Ivan Todorov <iwan.todorow@gmail.com>
2021-10-06 18:12:23 +0200

    Exercises-From-InterfacesAndAbstraction-Exercise

--- C#OOP/InterfacesAndAbstraction/InterfacesAndAbstraction-Exercise/PersonInfo/Citizen.cs
+++ C#OOP/InterfacesAndAbstraction/InterfacesAndAbstraction-Exercise/PersonInfo/Citizen.cs
@@ -4,16 +4,19 @@ using System.Text;
 
 namespace PersonInfo
 {
-    public class Citizen : IPerson
+    public class Citizen : IPerson,IBirthable,IIdentifiable
     {
-        public Citizen(string name,int age)
+        public Citizen(string name,int age,string id,string birthdate)
         {
             this.Name = name;
             this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthdate;
         }
 
         public string Name { get; set; }
         public int Age { get; set; }
-      
+        public string Birthdate { get ; set; }
+        public string Id { get; set; }
     }
 }
