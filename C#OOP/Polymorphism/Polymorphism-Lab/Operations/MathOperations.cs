fe4b15cb33734db74dc3abfd05405b0511daea58
Ivan Todorov <iwan.todorow@gmail.com>
2021-10-13 17:40:49 +0200

    Exercises-From-Polymorphism-Lab

---
+++ C#OOP/Polymorphism/Polymorphism-Lab/Operations/MathOperations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int firstNum, int secNum)
+        {        
+            return firstNum + secNum;
+        }
+        public double Add(double firstNum, double secNum, double thirdNum)
+        {
+            return firstNum + secNum + thirdNum;
+        }
+        public decimal Add(decimal firstNum, decimal secNum, decimal thirdNum)
+        {
+            return firstNum + secNum + thirdNum;
+        }
+
+
+    }
+}
