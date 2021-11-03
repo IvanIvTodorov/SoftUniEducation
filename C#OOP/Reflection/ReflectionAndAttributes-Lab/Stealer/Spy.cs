3ca00fde8396bc727f38a41009f7135d20266bd2
Ivan Todorov <iwan.todorow@gmail.com>
2021-11-03 18:17:10 +0100

    Exercises-From-Reflection-Lab

---
+++ C#OOP/Reflection/ReflectionAndAttributes-Lab/Stealer/Spy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+
+        public string StealFieldInfo(string name, params string[] parameters)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var fields = Type.GetType(name).GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            var instance = Activator.CreateInstance(Type.GetType(name), new object[] { });
+
+            sb.AppendLine($"Class under investigation: {name}");
+            foreach (var field in fields.Where(f => parameters.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(instance)}");
+               
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C#OOP/Reflection/ReflectionAndAttributes-Lab/Stealer/StartUp.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Stealer
+{
+    public  class StartUp
+    { 
+        static void Main(string[] args)
+        {
+
+            Spy spy = new Spy();
+            string className = typeof(Hacker).FullName;
+            string result = spy.StealFieldInfo(className, "username" ,"password" );
+            Console.WriteLine(result);
+
+        }
+    }
+}
