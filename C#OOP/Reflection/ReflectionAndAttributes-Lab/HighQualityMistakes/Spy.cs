db0efb9b69a9ed298f954bf92267e9ee4ee4397b
Ivan Todorov <iwan.todorow@gmail.com>
2021-11-03 19:13:01 +0100

    Exercises-From-Reflection-Lab

---
+++ C#OOP/Reflection/ReflectionAndAttributes-Lab/HighQualityMistakes/Spy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace HighQualityMistakes
+{
+    public class Spy
+    {
+        public string AnalyzeAccessModifiers(string className)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var fields = Type.GetType(className).GetFields(BindingFlags.Instance | BindingFlags.Public);
+
+            foreach (var field in fields)
+            {
+                sb.AppendLine($"{field.Name} must be private!");
+            }
+            var methods = Type.GetType(className).GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            foreach (var method in methods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} must be public!");
+            }
+
+            methods = Type.GetType(className).GetMethods(BindingFlags.Instance | BindingFlags.Public);
+
+            foreach (var method in methods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} must be private!");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C#OOP/Reflection/ReflectionAndAttributes-Lab/HighQualityMistakes/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+namespace HighQualityMistakes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string type = typeof(Hacker).FullName;
+            string result = spy.AnalyzeAccessModifiers(type);
+            Console.WriteLine(result);
+        }
+    }
+}
