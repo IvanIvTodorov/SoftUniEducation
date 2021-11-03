b76f2a7d4c71d429dc06edf77acfaf52cf6ddbbe
Ivan Todorov <iwan.todorow@gmail.com>
2021-11-03 19:34:13 +0100

    Exercises-From-ReflectionAndAttributes-Lab

---
+++ C#OOP/Reflection/ReflectionAndAttributes-Lab/MissionPrivateImpossible/Spy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace MissionPrivateImpossible
+{
+    public class Spy
+    {
+
+        public string RevealPrivateMethods(string className)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var methods = Type.GetType(className).GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            sb.AppendLine($"All Private Methods of Class: {className}");
+            sb.AppendLine($"Base Class: {Type.GetType(className).BaseType.Name}");
+            foreach (var method in methods)
+            {
+                sb.AppendLine(method.Name);
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
---
+++ C#OOP/Reflection/ReflectionAndAttributes-Lab/MissionPrivateImpossible/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MissionPrivateImpossible
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            Spy spy = new Spy();
+            string type = typeof(Hacker).FullName;
+            string result = spy.RevealPrivateMethods(type);
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
