970d764e202b60cca682324fac38e8a9498089c8
Ivan Todorov <iwan.todorow@gmail.com>
2021-11-04 18:13:00 +0100

    Exercises-From-ReflectionAndAttributes-Lab

---
+++ C#OOP/Reflection/ReflectionAndAttributes-Lab/Collector/Spy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Collector
+{
+    public class Spy
+    {
+        public string GetMethods()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var type = typeof(Hacker);
+
+            var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+            foreach (var method in methods.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} will return {method.ReturnType.FullName}");
+            }
+            var instance = Activator.CreateInstance(type, new object[] { });
+            foreach (var method in methods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} will set field {method.GetParameters().First().ParameterType}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C#OOP/Reflection/ReflectionAndAttributes-Lab/Collector/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Collector
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            Spy spy = new Spy();
+            string result = spy.GetMethods();
+            Console.WriteLine(result);
+        }
+    }
+}
