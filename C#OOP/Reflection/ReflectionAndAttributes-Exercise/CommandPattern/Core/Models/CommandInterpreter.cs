c871eb6e58e2476aeee89ef0dfa6a12f36a92930
Ivan Todorov <iwan.todorow@gmail.com>
2021-11-06 09:34:19 +0100

    Exercises-From-ReflectionAndAttributes-Exercise

---
+++ C#OOP/Reflection/ReflectionAndAttributes-Exercise/CommandPattern/Core/Models/CommandInterpreter.cs
@@ -0,0 +1,31 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CommandPattern
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string ADD_TO_NAME = "Command";
+        public string Read(string args)
+        {
+            string startWith = args.Split(" ",StringSplitOptions.RemoveEmptyEntries)[0] + ADD_TO_NAME;
+
+
+            var types = Assembly.GetCallingAssembly().GetTypes();
+            var type = types.Where(n => n.IsClass).First(n => n.Name == startWith);
+
+            if (type == null)
+            {
+                throw new ArgumentException("Invalid command name!");
+            }
+
+            var instance = (ICommand)Activator.CreateInstance(type);
+           
+            return instance.Execute(args.Split().Skip(1).ToArray());
+        }
+    }
+}
---
+++ C#OOP/Reflection/ReflectionAndAttributes-Exercise/CommandPattern/Core/Models/Engine.cs
@@ -0,0 +1,37 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+        public void Run()
+        {
+            while (true)
+            {
+                try
+                {
+                    string input = Console.ReadLine();
+                    string result = commandInterpreter.Read(input);
+                    Console.WriteLine(result);
+                }
+                catch (ArgumentException ae )
+                {
+
+                    Console.WriteLine(ae.Message);
+                }
+                
+            }
+            
+        }
+    }
+}
