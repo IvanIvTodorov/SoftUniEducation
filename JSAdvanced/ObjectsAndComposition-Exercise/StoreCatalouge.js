2d75a3121531e77630785d694a7b5773f8e12bd7
Ivan Todorov <iwan.todorow@gmail.com>
2022-01-15 23:53:25 +0200

    ObjectsAndComposition-Exercise

---
+++ JSAdvanced/ObjectsAndComposition-Exercise/StoreCatalouge.js
@@ -0,0 +1,34 @@
+function storeTheItems(arr) {
+    const collection = [];
+    for (const item of arr) {
+
+        let [product, price] = item.split(" : ");
+
+        collection.push({ product, price: Number(price) })
+    }
+    collection.sort((a, b) => a.product.localeCompare(b.product));
+
+    let compare = collection[0].product[0];
+    console.log(compare);
+    console.log(`  ${collection[0].product}: ${collection[0].price}`)
+    collection.splice(0, 1);
+    for (const iterator of collection) {
+        if (iterator.product[0] == compare) {
+            console.log(`  ${iterator.product}: ${iterator.price}`)
+        } else {
+            compare = iterator.product[0];
+            console.log(compare);
+            console.log(`  ${iterator.product}: ${iterator.price}`);
+        }
+    }
+}
+
+storeTheItems(['Appricot : 20.4',
+    'Fridge : 1500',
+    'TV : 1499',
+    'Deodorant : 10',
+    'Boiler : 300',
+    'Apple : 1.25',
+    'Anti-Bug Spray : 15',
+    'T-Shirt : 10']
+)
