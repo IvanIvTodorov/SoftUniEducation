474d7c59be358dc9af6a37412fcfda0d0b12a167
Ivan Todorov <iwan.todorow@gmail.com>
2022-01-19 18:44:28 +0200

    DOMIntroduction-Exercise

--- JSAdvanced/DOMIntroduction-Exercise/07. Hell's Kitchen/app.js
+++ JSAdvanced/DOMIntroduction-Exercise/07. Hell's Kitchen/app.js
@@ -3,7 +3,7 @@ function solve() {
 
    function onClick() {
       let input = document.querySelector('textarea').value;
-      let splited = input.split(/[\[\"\]]+/g).filter(x => x && x != ',');
+      let splited = input.split(/[\'\[\"\]]+/g).filter(x => x && x != ',');
       let restaurantCol = [];
 
       for (const restaurant of splited) {
@@ -17,7 +17,7 @@ function solve() {
                workers: []
             });
          };
-         
+
          for (let index = 0; index < workers.length; index += 2) {
 
             let workName = workers[index];
@@ -26,25 +26,43 @@ function solve() {
             const currentRest = restaurantCol.find(x => x.restName === name);
             currentRest.workers.push({
                name: workName,
-               salary: workSal
+               salary: Number(workSal)
             });
+            currentRest.workers.sort((a,b) => b.salary - a.salary);
          }
+         
       }
+
       let compare = 0;
       let bestName = '';
-      for (const obj of restaurantCol) {  
-      let current = 0;
+      for (const obj of restaurantCol) {
+         let current = 0;
          for (const arr of obj.workers) {
-            
+
             current += arr.salary;
          }
+
+         current /= obj.workers.length;
+
          if (current > compare) {
             compare = current;
             bestName = obj.restName;
          }
       }
-   //.workers.sort((a, b) => a.salary - b.salary);
-      // SMetni sus sort kato vzemesh areq ! s best name !!
-     
+
+      let best = restaurantCol.filter(n => n.restName == bestName);
+
+      let arrOfNames = [];
+      console.log(arrOfNames);
+      let bestSal = 0;
+      for (const obj of Object.entries(best)) {
+         bestSal = obj[1].workers[0].salary;
+         for (const secObj of obj[1].workers) {
+            arrOfNames.push(`Name: ${secObj.name} With Salary: ${secObj.salary}`);
+         }
+      }
+
+      document.getElementById('bestRestaurant').getElementsByTagName('p')[0].textContent = `Name: ${bestName} Average Salary: ${compare.toFixed(2)} Best Salary: ${bestSal.toFixed(2)}`
+      document.getElementById('workers').getElementsByTagName('p')[0].textContent = arrOfNames.join(" ");
    }
 }
