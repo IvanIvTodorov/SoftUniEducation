b8abc0d36504c45a2339ccfe1f6439f6fae376d5
Ivan Todorov <iwan.todorow@gmail.com>
2022-01-23 13:14:43 +0200

    DOMManipulationАndEvents-Exercise

--- JSAdvanced/DOMManipulationАndEvents-Exercise/08. Sudomu/solution.js
+++ JSAdvanced/DOMManipulationАndEvents-Exercise/08. Sudomu/solution.js
@@ -1,4 +1,82 @@
 function solve() {
+    
+    let tr = document.getElementsByTagName('tr');
+    let rows = Array.from(tr).splice(2, 3);
+    
+    let border = document.getElementsByTagName('table')[0];
+    let result = document.getElementById('check').firstElementChild;
 
-    //TODO...
+    let buttons = document.getElementsByTagName('button');
+    let clearBtn = buttons[1];
+    let checkBtn = buttons[0];
+
+    clearBtn.addEventListener('click', clearTable);
+
+    function clearTable(){
+        for (let index = 0; index < rows.length; index++) {
+            let row  = rows[index];
+            for (let cell = 0; cell < row.cells.length; cell++) {
+                row.cells[cell].firstElementChild.value = '';
+            }
+        }
+
+        border.style.border = 'none';
+        result.textContent = '';
+    }
+
+    checkBtn.addEventListener('click', checkTable);
+
+    function checkTable(){
+        let flag = true;
+
+        for (let index = 0; index < rows.length; index++) {
+            let row  = rows[index];
+            let compare = row.cells[0].firstElementChild.value;
+            if(compare > 3){
+                flag = false;
+                break;
+            }
+            for (let cell = 1; cell < row.cells.length; cell++) {
+                let element = row.cells[cell].firstElementChild.value
+
+                if (compare == element || element > 3) {
+                    flag = false
+                    break;
+                }
+            }
+            if (!flag) {
+                break;
+            }
+        }
+        if (flag) {
+            for (let col = 0; col < rows.length; col++) {
+                let compare = rows[0].cells[col].firstElementChild.value;
+                if(compare > 3){
+                    flag = false;
+                    break;
+                }
+               for (let row = 1; row < rows.length; row++) {
+                   let element = rows[row].cells[col].firstElementChild.value;
+                   
+                   if (compare == element || element > 3) {
+                       flag = false;
+                       break;
+                   }
+               }
+                if (!flag) {
+                    break;
+                }
+            }
+        }
+
+        if (flag) {
+            border.style.border = '2px solid green';
+            result.style.color = 'green';
+            result.textContent = 'You solve it! Congratulations!';
+        }else{
+            border.style.border = '2px solid red';
+            result.style.color = 'red';
+            result.textContent = 'NOP! You are not done yet...';
+        }
+    }
 }
--- JSAdvanced/DOMManipulationАndEvents-Exercise/09. JavaScript-Quiz/solution.js
+++ JSAdvanced/DOMManipulationАndEvents-Exercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,36 @@
 function solve() {
-  //TODO...
+
+  let answers = document.getElementsByClassName('answer-wrap');
+
+  for (const answer of Array.from(answers)) {
+    answer.addEventListener('click', onClick);
+  }
+
+  let blocks = document.getElementsByTagName('section');
+  let result = document.getElementsByTagName('h1')[1];
+
+  let curBlock = 0;
+  let rightAnswers = 0;
+  function onClick(ev) {
+    let answer = ev.target.textContent;
+
+    if (answer == "onclick" || answer == 'A programming API for HTML and XML documents' || answer == 'JSON.stringify()') {
+      rightAnswers++;
+    }
+
+    blocks[curBlock].style.display = 'none';
+    curBlock++
+
+    if (curBlock < blocks.length) {
+      blocks[curBlock].style.display = 'block';
+    } else {
+      document.getElementById('results').style.display = 'block';
+
+      if (rightAnswers == blocks.length) {
+        result.textContent = 'You are recognized as top JavaScript fan!'
+      } else {
+        result.textContent = `You have ${rightAnswers} right answers`
+      }
+    }
+  }
 }
