0d160761eec2374e87785961fe86835ef9c59405
Ivan Todorov <iwan.todorow@gmail.com>
2022-01-27 21:19:25 +0200

    AdvancedFunctions-Exercise

--- JSAdvanced/AdvancedFunctions-Exercise/08. Next Article/script.js
+++ JSAdvanced/AdvancedFunctions-Exercise/08. Next Article/script.js
@@ -1,3 +1,13 @@
 function getArticleGenerator(articles) {
-    // TODO
+    let temp = articles;
+    let div = document.getElementById('content');
+
+    return function solve() {
+        let init = temp.shift();
+        if (init) {
+            let article = document.createElement('article');
+            article.textContent = init;
+            div.appendChild(article);
+        }
+    }
 }
