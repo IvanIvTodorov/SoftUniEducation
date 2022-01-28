834d9270b005e7a908fd510419373421274e2d7e
Ivan Todorov <iwan.todorow@gmail.com>
2022-01-28 13:33:37 +0200

    AdvancedFunctions-Exercise

--- JSAdvanced/AdvancedFunctions-Exercise/09. Central Cinema/solution.js
+++ JSAdvanced/AdvancedFunctions-Exercise/09. Central Cinema/solution.js
@@ -1,3 +1,95 @@
 function solve() {
-    // TODO
+    let divInputs = document.getElementById('container');
+
+    let name = divInputs.children[0];
+    let hall = divInputs.children[1];
+    let ticketPrice = divInputs.children[2];
+
+    let buttons = document.getElementsByTagName('button');
+
+    let onScreen = buttons[0];
+    onScreen.type = 'button';
+    onScreen.addEventListener('click', onClick);
+
+    let moviesSection = document.getElementById('movies');
+    let ul = moviesSection.children[1];
+
+    function onClick() {
+        if (name.value.length != 0 && hall.value.length != 0 && Number(ticketPrice.value)) {
+            let li = document.createElement('li');
+            let span = document.createElement('span');
+            let strong = document.createElement('strong');
+            let div = document.createElement('div');
+            let strongDiv = document.createElement('strong');
+            let input = document.createElement('input');
+            let btn = document.createElement('button');
+
+            span.textContent = name.value;
+            strong.textContent = `Hall: ${hall.value}`;
+            strongDiv.textContent = Number(ticketPrice.value).toFixed(2);
+            input.placeholder = 'Tickets Sold';
+            btn.textContent = 'Archive';
+
+            btn.addEventListener('click', archive);
+
+            li.appendChild(span);
+            li.appendChild(strong);
+            div.appendChild(strongDiv);
+            div.appendChild(input);
+            div.appendChild(btn);
+            li.appendChild(div);
+
+            ul.appendChild(li);
+        }
+
+        name.value = '';
+        hall.value = '';
+        ticketPrice.value = '';
+    }
+    let archiveUl = document.getElementById('archive').children[1];
+    function archive(ev) {
+        let ticketsSold = ev.target.previousElementSibling;
+        
+        if (Number(ticketsSold.value)) {
+            let name = ev.target.parentNode.parentNode.firstChild;
+            let liToDelete = ev.target.parentNode.parentNode;
+            let price = ev.target.parentNode.firstChild;
+            
+            let li = document.createElement('li');
+            let span = document.createElement('span');
+            let strong = document.createElement('strong');
+            let btn = document.createElement('button');
+
+            btn.addEventListener('click', del);
+
+            span.textContent = name.textContent;
+            strong.textContent = `Total amount: ${Number(ticketsSold.value * price.textContent).toFixed(2)}`
+            btn.textContent = 'Delete';
+
+            li.appendChild(span);
+            li.appendChild(strong);
+            li.appendChild(btn);
+
+            archiveUl.appendChild(li);
+            liToDelete.remove();       
+        }
+    }
+
+    function del(ev){
+        let element = ev.target.parentNode;
+
+        element.remove();
+    }
+
+    let clearBtn = buttons[1];
+    clearBtn.addEventListener('click' ,delAll);
+
+    function delAll(){
+        let all = archiveUl.getElementsByTagName('li');
+        let size = all.length;
+
+        for (let index = 0; index < size; index++) {
+          all[0].remove();       
+        }
+    }
 }
--- JSAdvanced/AdvancedFunctions-Exercise/10. Task Manager/app.js
+++ JSAdvanced/AdvancedFunctions-Exercise/10. Task Manager/app.js
@@ -1,3 +1,69 @@
 function solve() {
-    console.log("//TODO")
+    let task = document.getElementById('task');
+    let descrip = document.getElementById('description');
+    let date = document.getElementById('date');
+    let addBtn = document.getElementById('add');
+    addBtn.type = 'button';
+
+    addBtn.addEventListener('click', add);
+
+    let sections = document.getElementsByTagName('section');
+    let open = sections[1];
+    let inProgress = sections[2];
+    let complete = sections[3];
+    
+    function add() {
+        if (task.value && descrip.value && date.value) {
+            let article = document.createElement('article');
+            let h3 = document.createElement('h3');
+            let firstP = document.createElement('p');
+            let secP = document.createElement('p');
+            let div = document.createElement('div');
+            let firstBtn = document.createElement('button');
+            let secBtn = document.createElement('button');
+
+            h3.textContent = task.value;
+            firstP.textContent = `Description: ${descrip.value}`;
+            secP.textContent = `Due Date: ${date.value}`;
+            div.classList.add('flex');
+            firstBtn.classList.add('green');
+            firstBtn.textContent = 'Start';
+            firstBtn.addEventListener('click', move);
+            secBtn.classList.add('red');
+            secBtn.textContent = 'Delete';
+            secBtn.addEventListener('click', del);
+            div.appendChild(firstBtn);
+            div.appendChild(secBtn);
+
+            article.appendChild(h3);
+            article.appendChild(firstP);
+            article.appendChild(secP);
+            article.appendChild(div);
+
+            open.children[1].appendChild(article);
+
+            function move(ev) {
+                let element = ev.target.parentNode.parentNode;
+                let current = element.getElementsByTagName('div')[0];
+                if (ev.target.textContent == 'Start') {
+                    ev.target.remove();
+                    current.appendChild(ev.target);
+
+                    ev.target.classList.remove('green');
+                    ev.target.classList.add('orange');
+                    ev.target.textContent = 'Finish';
+
+                    inProgress.children[1].appendChild(element);
+                } else {
+                    current.remove();
+                    complete.children[1].appendChild(element);
+                }
+            };
+            function del(ev) {
+                let element = ev.target.parentNode.parentNode;
+
+                element.remove();
+            }
+        }
+    }
 }
